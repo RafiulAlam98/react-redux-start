590e70fea160ea6c42265b2b7ba50031e715280a
M Rafiul Alam Tonmoy <mrafiul.alam7@gmail.com>
2023-07-24 15:50:20 +0600

    DONE: Redux setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { decrement, increment } from "./redux/features/counter/counterSlice";
+import { RootState } from "./redux/store/store";
+import { useSelector, useDispatch } from "react-redux";
+
 function App() {
+  const { count } = useSelector((state: RootState) => state.counter);
+  const dispatch = useDispatch();
   return (
     <>
-      <h2>tech net</h2>
+      <div className="bg-red-400">
+        <button className="red-300" onClick={() => dispatch(increment())}>
+          Increment
+        </button>
+      </div>
+      <div>{count}</div>
+      <div className="bg-red-400">
+        <button className="red-300" onClick={() => dispatch(decrement())}>
+          Increment
+        </button>
+      </div>
     </>
   );
 }
